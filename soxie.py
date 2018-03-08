1d2e2c09b68dae4925ed2d5f9d01c07e4e23c2b2
amakin1 <34159849+amakin1@users.noreply.github.com>
2018-03-08 22:49:41 +0800

    gohome sort dict

--- soxie.py
+++ soxie.py
@@ -6,13 +6,16 @@ Created on 2018年2月27日
 '''
 
 import urllib,urllib2,bs4
+import sys
 import ssl
 import re
 import time
 import xlwt
 import copy
+import json
 from bs4 import BeautifulSoup
 
+local_decode_type = sys.getfilesystemencoding()
 shop_list = []
 shop_info_dict = {}
 shop_pd_list = []
@@ -102,24 +105,24 @@ def pt_get_shop_list_page(url,shop_list,data_dict):
     shop_list = soup.find(class_='sjlist').find_all(class_ = 'message')
     shop_cnt_page = len(shop_list)
     for x in xrange(shop_cnt_page):
-        shop['name'] = shop_list[x].find(class_='storename').get_text()
-        shop['area'] = shop_list[x].find_all(class_='orange appr')[0].em.string
-        shop['class'] = shop_list[x].find_all(class_='orange appr')[1].em.string
-        shop['phone'] = shop_list[x].find_all(class_='detmsg')[0].font.string
-        shop['age'] = shop_list[x].find_all(class_='detmsg')[1].font.string
-        shop['qq'] = shop_list[x].find_all(class_='detmsg')[2].font.next_sibling.next_sibling.get_text().encode('utf-8').split(' ')[0]
-        shop['qq'] = shop['qq'].decode('utf-8')
-        shop['addr'] = shop_list[x].find(class_='detaddr').string
-        shop['website'] = shop_list[x].a.get('href')
-        shop_name = shop['website'].split('//')[1].split('.')[0]
-        print '===shop %d======'%x
+        shop['1name'] = shop_list[x].find(class_='storename').get_text()
+        shop['2area'] = shop_list[x].find_all(class_='orange appr')[0].em.string
+        shop['5class'] = shop_list[x].find_all(class_='orange appr')[1].em.string
+        shop['3phone'] = shop_list[x].find_all(class_='detmsg')[0].font.string
+        shop['6age'] = shop_list[x].find_all(class_='detmsg')[1].font.string
+        shop['4qq'] = shop_list[x].find_all(class_='detmsg')[2].font.next_sibling.next_sibling.get_text().encode('utf-8').split(' ')[0]
+        shop['4qq'] = shop['4qq'].decode('utf-8')
+        shop['7addr'] = shop_list[x].find(class_='detaddr').string
+        shop['8website'] = shop_list[x].a.get('href')
+        shop_name = shop['8website'].split('//')[1].split('.')[0]
+        print '===page %s shop %d======'%(data_dict['Page'],x)
         for k,v in shop.iteritems():
             print k,v
             #add2ddict(shop_info_dict,'%s'%shop_name,k,v)
         shop_info_dict['%s'%shop_name] = copy.deepcopy(shop)
         
 def pt_get_all_shop_list():
-    print '===get all shop list==='
+    print '--- get all shop list ---'
     url = url_shop_list_entry
     pn = 2
     for x in xrange(pn):
@@ -129,13 +132,15 @@ def pt_get_all_shop_list():
         }
         pt_get_shop_list_page(url, shop_list, data_dict)
         
-def pt_get_shop_product(url):
-    print '--- get shop product ---'
+def pt_get_shop_product(url_shop_site):
+    global pd_count
     n = 0
-    shop_name = url.split('//')[1].split('.')[0]
+    shop_pd_list = []
+    shop_name = url_shop_site.split('//')[1].split('.')[0]
+    print '--- get shop %s product ---'%shop_name
     data_dict = {}
     data_ecd = urllib.urlencode(data_dict)
-    url = url
+    url = url_shop_site
     req = urllib2.Request(url,headers=pt_soxie_header)
     res = opener.open(req)
     resr = res.read()
@@ -161,7 +166,8 @@ def pt_get_shop_product(url):
                 'page':x,
                 }
             data_ecd = urllib.urlencode(data_dict)
-            url = url+'page.aspx'+'?'+data_ecd
+            url = url_shop_site+'/page.aspx?'+data_ecd
+            print url
             req = urllib2.Request(url,headers=pt_soxie_header)
             res = opener.open(req)
             resr = res.read()
@@ -173,48 +179,56 @@ def pt_get_shop_product(url):
                 shop_pd_list.append(pro)
                 n+=1
     shop_pd_dict['%s'%shop_name] = shop_pd_list
-    print '=== shop %s total product num %d ==='%(shop_name,n)
+    pd_count+=n
+    print '=== product num shop %s %d ,all shop %d==='%(shop_name,n,pd_count)
     
-def pt_get_prod_detail(url,):
-    print '--- get prod detail ---'
+def pt_get_prod_detail(url):
+    global pd_count_has_get
+    pd_count_has_get+=1
+    pd_dict = {}
+    print '--- get prod detail %d of %d---'%(pd_count_has_get,pd_count)
     shop_name = url.split('//')[1].split('.')[0]
     pd_id = url.split('/')[3].split('.')[0]
-    req = urllib2.Request(url,headers=pt_soxie_header)
-    res = opener.open(req)
-    resr = res.read()
-    soup = BeautifulSoup(resr,'html.parser')
-    pd_info = soup.select('.proinfo .infobox_h .infoline')
-    print len(pd_info)
-    pd_name = pd_info[0].get_text().strip()
-    pd_num = pd_info[1].strong.get_text()
-    pd_price = pd_info[2].em.get_text()
-    pd_hot_index = pd_info[3].select('strong')[0].get_text()
-    pd_update_time = pd_info[3].select('strong')[1].font.get_text()
-    pd_size = pd_info[4].select_one('#size').select('li')
-    pd_color = pd_info[5].select_one('#color').select('li')
-    size_list = []
-    color_list = []
-    pd_dict = {}
-    for x in xrange(len(pd_size)):
-        size_list.append(pd_size[x].get_text())
-    for x in xrange(len(pd_color)):
-        color_list.append(pd_color[x].get_text())
-    pd_dict['pd_name'] = pd_name
-    pd_dict['pd_num'] = pd_num
-    pd_dict['pd_price'] = pd_price
-    pd_dict['pd_hot_index'] = pd_hot_index
-    pd_dict['pd_update_time'] = pd_update_time
-    pd_dict['pd_size'] = size_list
-    pd_dict['pd_color'] = color_list
-    for k,v in pd_dict.iteritems():
-        print k,v
-    add2ddict(shop_pd_detail_dict,shop_name,pd_id,pd_dict)
+    try:
+        req = urllib2.Request(url,headers=pt_soxie_header)
+        res = opener.open(req)
+        resr = res.read()
+        soup = BeautifulSoup(resr,'html.parser')
+        pd_info = soup.select('.proinfo .infobox_h .infoline')
+        print len(pd_info)
+        pd_name = pd_info[0].get_text().strip()
+        pd_num = pd_info[1].strong.get_text()
+        pd_price = pd_info[2].em.get_text()
+        pd_hot_index = pd_info[3].select('strong')[0].get_text()
+        pd_update_time = pd_info[3].select('strong')[1].font.get_text()
+        pd_size = pd_info[4].select_one('#size').select('li')
+        pd_color = pd_info[5].select_one('#color').select('li')
+        size_list = []
+        color_list = []
+        pd_dict = {}
+        for x in xrange(len(pd_size)):
+            size_list.append(pd_size[x].get_text())
+        for x in xrange(len(pd_color)):
+            color_list.append(pd_color[x].get_text())
+        pd_dict['pd_1name'] = pd_name
+        pd_dict['pd_2num'] = pd_num
+        pd_dict['pd_4price'] = pd_price
+        pd_dict['pd_3hot_index'] = pd_hot_index
+        pd_dict['pd_5update_time'] = pd_update_time
+        pd_dict['pd_6size'] = size_list
+        pd_dict['pd_7color'] = color_list
+        pd_dict['pd_8href'] = url
+        for k,v in pd_dict.iteritems():
+            print k,v
+        add2ddict(shop_pd_detail_dict,shop_name,pd_id,pd_dict)
+    except:
+        print 'get pd %s error'%url
     
 def pt_save_shop_pd_detail_data(data_dict):
     pt_time_init()
     pd_x =2
     pd_y =1
-    '''
+    #'''
     with open('.\data\pt_sooxie_%s.txt'%time_tag,mode = 'a+') as f:
         for k,v in data_dict.iteritems():
             if isinstance(v,dict):
@@ -223,7 +237,7 @@ def pt_save_shop_pd_detail_data(data_dict):
                     f.writelines(line)
             else:
                 print 'no pd'
-    '''
+    #'''
     pattern = xlwt.Pattern() # Create the Pattern
     pattern.pattern = xlwt.Pattern.SOLID_PATTERN # May be: NO_PATTERN, SOLID_PATTERN, or 0x00 through 0x12
     pattern.pattern_fore_colour = 3
@@ -231,23 +245,22 @@ def pt_save_shop_pd_detail_data(data_dict):
     style.pattern = pattern # Add Pattern to Style
     wkb = xlwt.Workbook()
     sheet_pd = wkb.add_sheet(u'商品信息')
-    '''
+    #'''
     sheet_pd.write(1,1,u'商家',style)
-    sheet_pd.write(1,2,u'商品链接',style)
-    sheet_pd.write(1,3,u'价格',style)
-    sheet_pd.write(1,4,u'货号',style)
-    sheet_pd.write(1,5,u'颜色',style)
+    sheet_pd.write(1,2,u'商品名称',style)
+    sheet_pd.write(1,3,u'货号',style)
+    sheet_pd.write(1,4,u'价格',style)
+    sheet_pd.write(1,5,u'热度',style)
     sheet_pd.write(1,6,u'更新时间',style)
     sheet_pd.write(1,7,u'尺码',style)
-    sheet_pd.write(1,8,u'名称',style)
-    sheet_pd.write(1,9,u'热度',style)
+    sheet_pd.write(1,8,u'颜色',style)
+    sheet_pd.write(1,9,u'链接',style)
+    data_dict.sort()
     for k,v in data_dict.iteritems():
         if isinstance(v,dict):
             for kk,vv in v.iteritems():
                 if isinstance(vv,dict):
                     sheet_pd.write(pd_x,pd_y,k)
-                    pd_y+=1
-                    sheet_pd.write(pd_x,pd_y,kk)
                     pd_y+=1
                     for kkk,vvv in vv.iteritems():
                         if isinstance(vvv,list):
@@ -262,23 +275,24 @@ def pt_save_shop_pd_detail_data(data_dict):
                 pd_y= 1
                 pd_x+=1
                 line = '%s:%s:%s \n'%(k,kk,vv)
-    '''
+    #'''
     sheet_shop = wkb.add_sheet(u'商家信息')
     pd_x =2
     pd_y =1
     sheet_shop.write(1,1,u'商家简称',style)
     sheet_shop.write(1,2,u'商家名称',style)
     sheet_shop.write(1,3,u'区域',style)
-    sheet_shop.write(1,4,u'等级',style)
-    sheet_shop.write(1,5,u'电话',style)
-    sheet_shop.write(1,6,u'年份',style)
-    sheet_shop.write(1,7,u'QQ',style)
+    sheet_shop.write(1,4,u'电话',style)
+    sheet_shop.write(1,5,u'QQ',style)
+    sheet_shop.write(1,6,u'等级',style)
+    sheet_shop.write(1,7,u'年份',style)
     sheet_shop.write(1,8,u'地址',style)
     sheet_shop.write(1,9,u'网址',style)
     lenn = len(shop_info_dict)
+    shop_info_dict.sort()
     for k,v in shop_info_dict.iteritems():
         if isinstance(v,dict):
-            print k,v
+            print k,v #(json.dumps(v)).decode('utf-8').encode(local_decode_type)
             sheet_shop.write(pd_x,pd_y,k)
             pd_y+=1
             for kk,vv in v.iteritems():
@@ -292,10 +306,14 @@ def pt_save_shop_pd_detail_data(data_dict):
 
 def main():
     print '---main func---'
+    global pd_count,pd_count_has_get
+    pd_count = 0
+    pd_count_has_get = 0
     pt_build_https_opener()
     #pt_get_shop_list_page_pn()
-    pt_get_all_shop_list()
+    pt_get_all_shop_list() #gen shop_info_dict
     #url = url_shop_list_entry
+    '''
     url = 'https://lingxiu.sooxie.com/'
     data_dict = {
         'r':'all',
@@ -303,13 +321,33 @@ def main():
     #pt_get_shop_list_page(url, shop_list, data_dict)
     pt_get_shop_product(url)
     '''
-    for k,v in shop_pd_dict.iteritems():
-        if isinstance(v,list):
-            for x in xrange(len(v)):
-                id = v[x]
-                pt_get_prod_detail(id)
-    '''
-    pt_get_prod_detail('https://hengjia.sooxie.com/288811.aspx')
+    
+    #'''
+    if True:
+        n = 0
+        for k,v in shop_info_dict.iteritems():
+            if n == 2:
+                break
+            if isinstance(v,dict):
+                shop_site = v['8website']
+                pt_get_shop_product(shop_site) #gen shop_pd_dict
+            n+=1
+    else:
+        pt_get_shop_product('https://tuhaocx.sooxie.com/')
+    
+    if True:    
+        for k,v in shop_pd_dict.iteritems():
+        #'''
+            if isinstance(v,list):
+                for x in xrange(50):
+                    id = v[x]
+                    print '\n'
+                    pt_get_prod_detail(id)
+                    #'''
+    else:
+        pt_get_prod_detail('https://hengjia.sooxie.com/288811.aspx')
+    #'''
+    
     pt_save_shop_pd_detail_data(shop_pd_detail_dict)
     pass
 
